5d1e229b5bf75e492143eaeb42eae9c45f06e6f4
iar <38orlov@gmail.com>
2025-05-01 14:16:38 +0500

    фикс импортов

--- src/lunch/core/storages.py
+++ src/lunch/core/storages.py
@@ -1,6 +1,6 @@
 import datetime
 
-from core import db, entities
+from core import in_memory_db, entities
 
 
 class ObjectDoesNotExistsError(Exception):
@@ -9,13 +9,13 @@ class ObjectDoesNotExistsError(Exception):
 
 class UserInMemoryStorage:
     def get_all(self, offset: int = 0, limit: int | None = None) -> list[entities.User]:
-        users = sorted(db.users, key=lambda user: user.joined_dt, reverse=True)
+        users = sorted(in_memory_db.users, key=lambda user: user.joined_dt, reverse=True)
         if limit is None:
             return users
         return users[offset : offset + limit]
 
     def get_by_id(self, user_id: int) -> entities.User | None:
-        for user in db.users:
+        for user in in_memory_db.users:
             if user.id == user_id:
                 break
         else:
@@ -23,7 +23,7 @@ class UserInMemoryStorage:
         return user
 
     def get_by_username(self, username: str) -> entities.User | None:
-        for user in db.users:
+        for user in in_memory_db.users:
             if user.username == username:
                 break
         else:
@@ -31,8 +31,8 @@ class UserInMemoryStorage:
         return user
 
     def add(self, user: entities.User) -> entities.User:
-        user.id = max([user.id for user in db.users], default=0) + 1
-        db.users.append(user)
+        user.id = max([user.id for user in in_memory_db.users], default=0) + 1
+        in_memory_db.users.append(user)
         return user
 
     def update(self, user: entities.User) -> entities.User:
@@ -47,7 +47,7 @@ class UserInMemoryStorage:
         return user_from_db
 
     def get_count(self, **kwargs: str | int | None) -> int:
-        users = db.users
+        users = in_memory_db.users
         for key, value in kwargs.items():
             users = [user for user in users if getattr(user, key) == value]
         return len(users)
@@ -55,13 +55,13 @@ class UserInMemoryStorage:
 
 class OrderInMemoryStorage:
     def get_all(self, offset: int = 0, limit: int | None = None) -> list[entities.Order]:
-        orders = sorted(db.orders, key=lambda order: order.date, reverse=True)
+        orders = sorted(in_memory_db.orders, key=lambda order: order.date, reverse=True)
         if limit is None:
             return orders
         return orders[offset : offset + limit]
 
     def get_by_id(self, order_id: int) -> entities.Order | None:
-        for order in db.orders:
+        for order in in_memory_db.orders:
             if order.id == order_id:
                 break
         else:
@@ -69,26 +69,26 @@ class OrderInMemoryStorage:
         return order
 
     def get_by_user_id(self, user_id: int, offset: int = 0, limit: int | None = None) -> list[entities.Order]:
-        user_orders = [order for order in db.orders if order.user_id == user_id]
+        user_orders = [order for order in in_memory_db.orders if order.user_id == user_id]
         sorted_user_orders = sorted(user_orders, key=lambda order: order.date, reverse=True)
         if limit is None:
             return sorted_user_orders
         return sorted_user_orders[offset : offset + limit]
 
     def get_by_date(self, date: datetime.date, offset: int = 0, limit: int | None = None) -> list[entities.Order]:
-        date_orders = [order for order in db.orders if order.date == date]
+        date_orders = [order for order in in_memory_db.orders if order.date == date]
         sorted_date_orders = sorted(date_orders, key=lambda order: order.date, reverse=True)
         if limit is None:
             return sorted_date_orders
         return sorted_date_orders[offset : offset + limit]
 
     def get_by_user_id_and_date(self, user_id: int, date: datetime.date) -> list[entities.Order]:
-        user_date_orders = [order for order in db.orders if order.user_id == user_id and order.date == date]
+        user_date_orders = [order for order in in_memory_db.orders if order.user_id == user_id and order.date == date]
         return sorted(user_date_orders, key=lambda order: order.date, reverse=True)
 
     def add(self, order: entities.Order) -> entities.Order:
-        order.id = max([order.id for order in db.orders], default=0) + 1
-        db.orders.append(order)
+        order.id = max([order.id for order in in_memory_db.orders], default=0) + 1
+        in_memory_db.orders.append(order)
         return order
 
     def update(self, order: entities.Order) -> entities.Order:
@@ -104,33 +104,33 @@ class OrderInMemoryStorage:
         order_from_db = self.get_by_id(order.id)
         if not order_from_db:
             raise ObjectDoesNotExistsError
-        db.orders.remove(order_from_db)
+        in_memory_db.orders.remove(order_from_db)
 
     def get_count(self, **kwargs: str | int | None) -> int:
-        orders = db.orders
+        orders = in_memory_db.orders
         for key, value in kwargs.items():
             orders = [order for order in orders if getattr(order, key) == value]
         return len(orders)
 
     def add_dishes(self, order: entities.Order, dishes: list[entities.Dish]) -> None:
         for dish in dishes:
-            order_dish = db.OrderDish(order_id=order.id, dish_id=dish.id)
-            db.order_dishes.append(order_dish)
+            order_dish = in_memory_db.OrderDish(order_id=order.id, dish_id=dish.id)
+            in_memory_db.order_dishes.append(order_dish)
 
     def clear_dishes(self, order: entities.Order) -> None:
-        for order_dish in db.order_dishes:
+        for order_dish in in_memory_db.order_dishes:
             if order_dish.order_id == order.id:
-                db.order_dishes.remove(order_dish)
+                in_memory_db.order_dishes.remove(order_dish)
 
 
 class DishInMemoryStorage:
     def get_all(self, offset: int = 0, limit: int | None = None) -> list[entities.Dish]:
         if limit is None:
-            return db.dishes
-        return db.dishes[offset : offset + limit]
+            return in_memory_db.dishes
+        return in_memory_db.dishes[offset : offset + limit]
 
     def get_by_id(self, dish_id: int) -> entities.Dish | None:
-        for dish in db.dishes:
+        for dish in in_memory_db.dishes:
             if dish.id == dish_id:
                 break
         else:
@@ -138,12 +138,12 @@ class DishInMemoryStorage:
         return dish
 
     def get_by_ids(self, dish_ids: list[int]) -> list[entities.Dish]:
-        return [dish for dish in db.dishes if dish.id in dish_ids]
+        return [dish for dish in in_memory_db.dishes if dish.id in dish_ids]
 
     def get_by_category_code(self, category_code: str, date: datetime.date | None = None) -> list[entities.Dish]:
         dishes_ids = [
             dish_category.dish_id
-            for dish_category in db.dishes_categories
+            for dish_category in in_memory_db.dishes_categories
             if dish_category.category_code == category_code
         ]
         dishes = [self.get_by_id(dish_id) for dish_id in dishes_ids]
@@ -152,7 +152,7 @@ class DishInMemoryStorage:
         return [dish for dish in dishes if not dish.weekday or dish.weekday == date.isoweekday()]
 
     def get_by_order_id(self, order_id: int) -> list[entities.Dish]:
-        dishes_ids = [order_dish.dish_id for order_dish in db.order_dishes if order_dish.order_id == order_id]
+        dishes_ids = [order_dish.dish_id for order_dish in in_memory_db.order_dishes if order_dish.order_id == order_id]
         return [self.get_by_id(dish_id) for dish_id in dishes_ids]
 
     def get_by_category_code_and_order_id(self, category_code: str, order_id: int) -> list[entities.Dish]:
